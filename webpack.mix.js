c0c49cfff8828478b10e9234b07ae5efe764d240
paul <redchik091089@yandex.ru>
2022-05-08 20:58:21 +0300

    added layouts and forming pages

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,26 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
+    .postCss('resources/css/app.css', 'public/css',[
+        require('postcss-custom-properties')
     ]);
+
+// mix.css([
+//     'resources/css/bootstrap.css',
+//     'resources/css/bootstrap-reboot.css',
+//     'resources/css/font-awesome.min.css',
+//     'resources/css/icomoon.css',
+//     'resources/css/jquery.fancybox.min.css',
+//     'resources/css/jquery.mCustomScrollbar.min.css',
+//     'resources/css/responsive.css',
+//     'resources/css/style.css'
+// ], 'public/css/styles.css');
+//
+// mix.js([
+//     'resources/js/bootstrap.bundle.min.js',
+//     'resources/js/custom.js',
+//     'resources/js/jquery.mCustomScrollbar.concat.min.js',
+//     'resources/js/jquery.min.js',
+//     'resources/js/jquery-3.0.0.min.js',
+//     'resources/js/popper.min.js'
+// ], 'public/js/scripts.js');
