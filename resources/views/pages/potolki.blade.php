35e1e0b62e7154997a04cc69111e7e173d38c78a
redchik091089 <redchik091089@yandex.ru>
2022-05-27 16:58:31 +0000

    modified potolok and product pages

--- resources/views/pages/potolki.blade.php
+++ resources/views/pages/potolki.blade.php
@@ -9,15 +9,47 @@
             <h1>{{ $potolok->prod_name }}</h1>
             <p>{{ $potolok->description }}</p>
         </div>
+             @isset($potolok->image_1)
         <div class="img_pot">
             <img src="{{ $potolok->image_1 }}" alt="#"/>
         </div>
-        <div class="img_pot">
+            @endisset
+            @isset($potolok->image_2)
+         <div class="img_pot">
             <img src="{{ $potolok->image_2 }}" alt="#"/>
         </div>
-        <div class="img_pot">
+            @endisset
+            @isset($potolok->image_3)
+            <div class="img_pot">
             <img src="{{ $potolok->image_3 }}" alt="#"/>
         </div>
+            @endisset
+            @isset($potolok->image_4)
+            <div class="img_pot">
+            <img src="{{ $potolok->image_4 }}" alt="#"/>
+        </div>
+            @endisset
+            @isset($potolok->image_5)
+            <div class="img_pot">
+            <img src="{{ $potolok->image_5 }}" alt="#"/>
+        </div>
+            @endisset
+            @isset($potolok->image_6)
+        <div class="img_pot">
+            <img src="{{ $potolok->image_6 }}" alt="#"/>
+        </div>
+            @endisset
+            @isset($potolok->image_7)
+        <div class="img_pot">
+            <img src="{{ $potolok->image_7 }}" alt="#"/>
+        </div>
+            @endisset
+            @isset($potolok->image_8)
+        <div class="img_pot">
+            <img src="{{ $potolok->image_8 }}" alt="#"/>
+        </div>
+            @endisset
+
         @endforeach
     </div>
 
--- resources/views/pages/products.blade.php
+++ resources/views/pages/products.blade.php
@@ -18,9 +18,11 @@
                 <div class="potolki-item">
                     @foreach($products as $product)
                         <div class="col-lg-4 col-md-4 col-sm-4 float">
-                            <a href="/single/{{ $product->id }}" rel="nofollow"><img src="{{ $product->image_1 }}" alt=""></a>
+                            <a href="/single/{{ $product->id }}" rel="nofollow"><img src=@if($product->id == 4 or $product->id == 5 or $product->id == 6 or $product->id == 15)"{{ $product->image_2 }}"@else"{{ $product->image_1 }}"@endif alt=""></a>
                             <h6><a href="/single/{{ $product->id }}">{{ $product->prod_name }}</a></h6>
+                            @isset($product->price)
                             <p class="nrdr2"><span>{{ $product->price }}</span> руб.</p>
+                            @endisset
                             {{--                        <a href="/primery/dvuxurovnevye-potolki-s-podsvetkoj/" rel="nofollow" class="order nrdr">Заказать</a>--}}
                         </div>
                     @endforeach
